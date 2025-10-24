2f8131b6b0e723e9173d98424bfeb6c2232d28d0
Arys <poluektovdmytro@gmail.com>
2025-10-24 00:14:18 -0700

    extended about page and added animations

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,43 +1,55 @@
-import React from 'react';
+import React  from 'react';
+import {AnimatedWrapper} from "@/app/components/Animations";
+
+const cards = [
+    {
+        id: 1,
+        title: "Dev. Tools",
+        content: ["HTML","CSS", "JS/TS","Tailwind","React", "Next.js", "Express", "Gsap"],
+        styles: "box-1 gap-8 "
+
+    },
+    {
+        id: 2,
+        title: "Languages ",
+        content: ["English","Ukrainian", "Russian"],
+        styles: "box-2 gap-2 "
+    },
+    {
+        id: 3,
+        title: "Other interests",
+        content: ["English","Ukrainian", "Russian","Russian"],
+        styles: "box-3 gap-6 "
+    },
+    {
+        id: 4,
+        title: "Education",
+        content: ["English","Ukrainian", "Russian"],
+        styles: "box-4 gap-2 "
+    },
+
+
+]
 
 const About = () => {
     return (
         <div className="flex justify-center items-center min-h-[calc(100vh-14rem)]">
-            <div className="grid-container w-80 md:w-[658px] " >
 
-                <div className="flex flex-col grid-area: box-1 gap-8 p-10 bg-bg w-80 rounded-2xl ">
-                    <h2 className="text-3xl font-bold ">Dev. Tools</h2>
-                    <div className="grid grid-cols-2 gap-x-8 gap-y-4">
-                        <span>HTML</span>
-                        <span>CSS</span>
-                        <span>JS</span>
-                        <span>TS</span>
-                        <span>React</span>
-                        <span>Next.js</span>
+            <AnimatedWrapper className="grid-container w-80 md:w-[658px] " >
+                {cards.map((card) => (
+                    <div key={card.id} className={`flex flex-col justify-center items-center grid-area: ${card.styles} p-6 bg-bg w-full rounded-2xl text-center`}>
+                        <h2 className="text-3xl font-bold ">{card.title}</h2>
+                        <div className={`${card.content.length > 4 ? "grid grid-cols-2 gap-x-8 gap-y-4": "flex flex-col gap-x-8 gap-y-4"}`}>
+                            {card.content.map((content, idx) => (
+                                <span key={idx} className="p-2 px-4 bg-bg-light rounded-2xl">{content}</span>
+                            ))}
+                        </div>
                     </div>
-                </div>
+                ))}
 
-                <div className="flex flex-col grid-area: box-2 gap-8 p-10 bg-bg w-80 rounded-2xl h-full">
-                    <h2 className="text-3xl font-bold ">Dev. Tools</h2>
-                    <div className="">
-                        <span>HTML/CSS</span>
-                        <span>JS/TS</span>
-                        <span>React</span>
-                        <span>Next.js</span>
-                    </div>
-                </div>
-
-                <div className="flex flex-col grid-area: box-3 gap-8 p-10 bg-bg w-80 rounded-2xl h-full">
-                    <h2 className="text-3xl font-bold ">Dev. Tools</h2>
-
-                </div>
-                <div className="flex flex-col grid-area: box-4 gap-8 p-10 bg-bg w-80 rounded-2xl h-full">
-                    <h2 className="text-3xl font-bold ">Dev. Tools</h2>
-
-                </div>
+            </AnimatedWrapper>
 
 
-            </div>
 
         </div>
     )
---
+++ app/components/Animations.tsx
@@ -0,0 +1,39 @@
+"use client";
+import gsap from "gsap";
+import React, { useRef, ReactElement } from "react";
+import { useGSAP } from "@gsap/react";
+
+type AnimatedWrapperProps = {
+    children: ReactElement | ReactElement[];
+    stagger?: number;
+    duration?: number;
+    yPercent?: number;
+    className?: string;
+};
+
+export const AnimatedWrapper = ({children, stagger = 0.2, duration = 0.7, yPercent = 100, className = "",}: AnimatedWrapperProps) => {
+    const itemsRef = useRef<(HTMLDivElement | null)[]>([]);
+
+    useGSAP(() => {
+        gsap.from(itemsRef.current, {
+            yPercent,
+            opacity: 0,
+            duration,
+            ease: "power3.out",
+            stagger,
+        });
+    }, [stagger, duration, yPercent]);
+
+    return (
+        <div className={className}>
+            {React.Children.map(children, (child, i) => {
+                if (React.isValidElement(child)) {
+                    return React.cloneElement(child as React.ReactElement<any>, {
+                        ref: (el: HTMLDivElement | null) => (itemsRef.current[i] = el),
+                    });
+                }
+                return child;
+            })}
+        </div>
+    );
+};
